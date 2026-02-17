e1f56472008c73c5caf37b9cf642ac4e9070feb4
lenoraaa <lenorarachelxavier.27csb@licet.ac.in>
2026-02-17 15:11:31 +0530

    initcom

---
+++ src/test/java/com/example/csetest/CsetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
